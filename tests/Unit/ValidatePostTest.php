6fffe59dcb11d286ae7ecae379ffd89c2b95215c
ludoguenet <ludovicguenet@gmx.com>
2023-01-28 23:43:53 +0100

    Used draft status in test

--- tests/Unit/ValidatePostTest.php
+++ tests/Unit/ValidatePostTest.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 use App\Actions\Post\ValidatePostAction;
 use App\Enums\PostStatusEnum;
+use App\Mail\PostValidatedMail;
 use App\Models\Post;
 
 it('validates a post', function () {
     \Illuminate\Support\Facades\Mail::fake();
-    $post = Post::factory()->create();
+    $post = Post::factory()->create([
+        'status' => PostStatusEnum::DRAFT,
+    ]);
 
     (new ValidatePostAction)(
         post: $post,
     );
 
-    \Illuminate\Support\Facades\Mail::assertSent(\App\Mail\PostValidatedMail::class);
+    \Illuminate\Support\Facades\Mail::assertSent(PostValidatedMail::class);
 
     expect($post->status)->toEqual(PostStatusEnum::VALIDATED);
     expect($post->user->total_points)->toEqual(10);
